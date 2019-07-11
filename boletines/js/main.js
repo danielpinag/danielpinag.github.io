6f2ee3e83d3d1eb50d749b84688bad7f2995febd
Daniel Piña <danielalexpg@gmail.com>
2019-07-11 10:27:07 -0500

    Agregamos proyecto de boletines

---
+++ boletines/js/main.js
@@ -0,0 +1,19 @@
+let close = document.querySelectorAll(".close");
+close.forEach(function (link) {
+	link.addEventListener("click", function(ev){
+		ev.preventDefault();
+
+		let content = document.querySelector(".content");
+
+		content.classList.remove("animated");
+		content.classList.remove("zoomIn");
+
+		content.classList.add("animated");
+		content.classList.add("zoomOut");
+
+		setTimeout(function(){
+			location.href = "/";
+		},600);
+	});
+});
+
